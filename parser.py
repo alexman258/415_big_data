38fdba030d91cb762acd5948a3d6c235c65a462a
alexman258 <alexman258@gmail.com>
2019-09-24 11:33:47 -0700

    Updated parsing to include more attributes

--- parser.py
+++ parser.py
@@ -1,40 +1,44 @@
 fname = "amazon-meta.txt"
-with open(fname, encoding = 'utf8') as f:
+with open(fname, encoding = 'ISO-8859-1') as f:
     content = f.readlines()
 #Remove the beginning and trailing white spaces.
 content = [x.strip() for x in content] 
 
-# Write extracted information to testfile.txt in a format of ',' demilited files.
-# The columns are Id, title, group, categories, totalreviews, avgrating.
-# The code stores all extracted information about a product into previoulines,
-# and write the content into file only when all information are available. Hence,
-# if review information for a product is not available, the product won't appear
-# in the final file.
-file = open("file.txt","w", encoding='utf8')
-previouslines = ['Id', 'title', 'group', 'categories', 'totalreviews', 'avgrating']
+file = open("file.csv","w", encoding='utf8')
+previouslines = ['Id', 'ASIN', 'title', 'group', 'salesrank', 'categories', 'totalreviews', 'avgrating']
 for line in content:
     lines = line.split(':')
-    if lines[0] == "Id":
-        if (len(previouslines) == 6):
-            for component in previouslines[0:5]:
+    if lines[0] == 'Id':
+        if(len(previouslines) == 8):
+            for component in previouslines[0:7]:
                 file.write(component)
                 file.write(',')
-            file.write(previouslines[5])
-            file.write("\n")
+            file.write(previouslines[7])
+            file.write('\n')
         previouslines = []
         previouslines.append(lines[1].strip())
-        
-    if lines[0] == "title":
-        title = ':'.join(lines[1:]).strip().replace(',', ' ').replace('\n', ' ').strip()
-        previouslines.append(title)
-       
-    if lines[0] == "group":
+    
+    if lines[0] == 'ASIN':
         previouslines.append(lines[1].strip())
 
-    if lines[0] == "categories":
+    if lines[0] == 'title':
+        title = ':'.join(lines[1:]).strip().replace(',', ' ').replace('\n', ' ').strip()
+        previouslines.append(title)
+
+    if lines[0] == 'group':
         previouslines.append(lines[1].strip())
-    
+
+    if lines[0] == 'salesrank':
+        previouslines.append(lines[1].strip())
+
+   #if lines[0] == 'similar':
+   #Add code to add list of similar categories
+
+    if lines[0] == 'categories':
+        previouslines.append(lines[1].strip())
+
     if lines[0] == "reviews" and lines[1].strip() == "total":
         previouslines.append(lines[2].split(' ')[1])
         previouslines.append(lines[4].strip())
+
 file.close()
